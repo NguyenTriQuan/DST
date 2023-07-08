5734bd0247aaab5f02279d1c1a44b0a317d00b38
Nguyễn Trí Quân <nguyentriquan99@gmail.com>
2023-07-08 20:22:23 +0700

    dst

--- sparselearning/models.py
+++ sparselearning/models.py
@@ -554,6 +554,7 @@ class BasicBlock(nn.Module):
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
         self.res = Residual()
+        self.relu = nn.ReLU()
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
@@ -562,11 +563,11 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out = self.res(out, self.shortcut(x))
         # out += self.shortcut(x)
-        out = F.relu(out)
+        out = self.relu(out)
         return out
 
 
