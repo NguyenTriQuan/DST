a5acc952785b8895898a00d5e30a7dea26d3c149
Nguyễn Trí Quân <nguyentriquan99@gmail.com>
2023-07-10 12:13:11 +0700

    dst

--- main.py
+++ main.py
@@ -106,16 +106,13 @@ def train(args, model, device, train_loader, optimizer, epoch, mask=None):
                     # print(grad.norm(2), grad.max(), grad.min())
                     reg += grad.abs().sum()
                     if len(grad.shape) == 4:
-                        # eff_nodes += torch.sign(grad.abs().sum((1,2,3))).sum()
-                        eff_nodes += grad.abs().sum((1,2,3)).sign().sum()
-                        # eff_nodes += NotZero.apply(NotZero.apply(grad).sum((1,2,3))).sum()
-                        # eff_nodes += NotZero.apply(grad.sum((1,2,3))).sum()
+                        temp = grad.sum((1,2,3))
+                        reg += temp.sum()
+                        eff_nodes += temp.sign().sum().item()
                     else:
-                        # eff_nodes += torch.sign(grad.abs().sum((1))).sum()
-                        eff_nodes += grad.abs().sum((1)).sign().sum()
-                        # eff_nodes += NotZero.apply(NotZero.apply(grad).sum((1))).sum()
-                        # eff_nodes += NotZero.apply(grad.sum((1))).sum()
-                # loss -= args.lamb * (args.alpha*eff_nodes.log() + (1-args.alpha)*eff_paths)
+                        temp = grad.sum((1))
+                        reg += temp.sum()
+                        eff_nodes += temp.sign().sum().item()
                 loss -= args.lamb * (args.alpha*reg + (1-args.alpha)*eff_paths)
                 # print(eff_nodes, eff_paths)
 
