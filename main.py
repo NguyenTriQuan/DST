ce1fc3675587c4d696db1b2dabe8d9f74545d7e4
Nguyễn Trí Quân <nguyentriquan99@gmail.com>
2023-07-10 16:49:41 +0700

    dst

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ def train(args, model, device, train_loader, optimizer, epoch, mask=None):
                 reg = 0
                 eff_nodes = 0
                 for grad in grad_dummy:
-                    # print(grad.numel(), grad.min(), grad.max())
+                    print(grad.shape, grad)
                     if len(grad.shape) == 4:
                         temp = grad.norm(2, dim=(0,2,3))
                         reg += temp.sum()
