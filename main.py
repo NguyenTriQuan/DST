dc62285a1ca67290f674814b235a0aebc64c2c25
Nguyễn Trí Quân <nguyentriquan99@gmail.com>
2023-07-11 12:33:15 +0700

    dst

--- main.py
+++ main.py
@@ -155,55 +155,55 @@ def train(args, model, device, train_loader, optimizer, epoch, mask=None):
     # with torch.cuda.amp.autocast(enabled=enabled):
     #     eff_nodes, eff_paths = measure_node_path(model)
 
-    # data = (torch.zeros((1, c, h, w)).float().cuda(), torch.zeros((1, c, h, w)).float().cuda())
-    # eff_paths, output = model(data)
-    # eff_paths = torch.logsumexp(eff_paths, dim=(0,1))
-    # dummies = []
-    # num_zeros = []
-    # for m in model.modules():
-    #     if hasattr(m, 'score'):
-    #         # dummies.append(m.eff_paths)
-    #         dummies.append(m.dummy)
-    #         num_zeros.append(m.num_zeros)
-    # grad_dummy = torch.autograd.grad(eff_paths, dummies)
-    # eff_params = 0
-    # eff_kernels = 0
-    # eff_nodes = 0
-    # total_nodes = 0
-    # total_params = 0
-    # for i, grad in enumerate(grad_dummy):
-    #     temp = grad != 0
-    #     if len(grad.shape) == 4:
-    #         eff_nodes += (grad.sum(dim=(1,2,3)) != 0).sum()
-    #         eff_kernels += (grad.sum((2,3)) != 0).sum()
-    #     else:
-    #         eff_nodes += (grad.sum(dim=(1)) != 0).sum()
-        
-    #     eff_params += temp.sum()
-    #     total_nodes += temp.shape[0]
-    #     total_params += temp.numel() - num_zeros[i]
-
-    # print(f'Eff nodes: {eff_nodes}/{total_nodes}, Eff paths: {eff_paths}, Eff kernels: {eff_kernels}, Eff params: {eff_params}/{total_params}')
-
     data = (torch.zeros((1, c, h, w)).float().cuda(), torch.zeros((1, c, h, w)).float().cuda())
     eff_paths, output = model(data)
     eff_paths = torch.logsumexp(eff_paths, dim=(0,1))
     dummies = []
+    num_zeros = []
     for m in model.modules():
         if hasattr(m, 'score'):
-            dummies.append(m.eff_paths)
+            # dummies.append(m.eff_paths)
+            dummies.append(m.dummy)
+            num_zeros.append(m.num_zeros)
     grad_dummy = torch.autograd.grad(eff_paths, dummies)
+    eff_params = 0
+    eff_kernels = 0
     eff_nodes = 0
-    total = 0
-    for grad in grad_dummy:
+    total_nodes = 0
+    total_params = 0
+    for i, grad in enumerate(grad_dummy):
+        temp = grad != 0
         if len(grad.shape) == 4:
-            temp = grad.norm(2, dim=(0,2,3))
+            eff_nodes += (grad.sum(dim=(1,2,3)) != 0).sum()
+            eff_kernels += (grad.sum((2,3)) != 0).sum()
         else:
-            temp = grad.norm(2, dim=(0))
+            eff_nodes += (grad.sum(dim=(1)) != 0).sum()
+        
+        eff_params += temp.sum()
+        total_nodes += temp.shape[0]
+        total_params += temp.numel() - num_zeros[i]
 
-        eps = (temp == 0)*1e-3
-        eff_nodes += torch.sum(temp / (temp + eps))
-        total += temp.shape[0]
+    print(f'Eff nodes: {eff_nodes}/{total_nodes}, Eff paths: {eff_paths}, Eff kernels: {eff_kernels}, Eff params: {eff_params}/{total_params}')
+
+    # data = (torch.zeros((1, c, h, w)).float().cuda(), torch.zeros((1, c, h, w)).float().cuda())
+    # eff_paths, output = model(data)
+    # eff_paths = torch.logsumexp(eff_paths, dim=(0,1))
+    # dummies = []
+    # for m in model.modules():
+    #     if hasattr(m, 'score'):
+    #         dummies.append(m.eff_paths)
+    # grad_dummy = torch.autograd.grad(eff_paths, dummies)
+    # eff_nodes = 0
+    # total = 0
+    # for grad in grad_dummy:
+    #     if len(grad.shape) == 4:
+    #         temp = grad.norm(2, dim=(0,2,3))
+    #     else:
+    #         temp = grad.norm(2, dim=(0))
+
+    #     eps = (temp == 0)*1e-3
+    #     eff_nodes += torch.sum(temp / (temp + eps))
+    #     total += temp.shape[0]
 
     print_and_log('\n{}: Average loss: {:.4f}, Accuracy: {}/{} ({:.3f}%), Eff nodes: {}/{}, Eff paths: {} \n'.format(
         'Training summary' ,
