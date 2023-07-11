3b5a089cbbffb305d28c2e048219921d3a3b9e34
Nguyễn Trí Quân <nguyentriquan99@gmail.com>
2023-07-11 13:49:10 +0700

    dst

--- sparselearning/core.py
+++ sparselearning/core.py
@@ -128,7 +128,6 @@ def NPB_conv_forward(self, x):
     if self.training:
         self.mask = TopK.apply(self.weight.abs(), self.num_zeros)
         eff_paths, images = x
-        print(x)
         max_paths = eff_paths.max()
         eff_paths = torch.log(self._conv_forward((eff_paths - max_paths).exp(), self.mask, None)+1e-3) + max_paths
         out = self._conv_forward(images, self.weight, self.bias)
@@ -427,7 +426,7 @@ class Masking(object):
         print('Removing 1D batch norms...')
         self.remove_type(nn.BatchNorm1d)
         
-        if self.args.method == 'score_npb':
+        if 'npb' in self.args.method:
             NPB_register(module, self.args)
 
         self.init(mode=sparse_init, density=density)
