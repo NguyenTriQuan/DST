4500eacc7517bf69408619fd7112eca065d26009
Nguyễn Trí Quân <nguyentriquan99@gmail.com>
2023-07-13 15:51:55 +0700

    dst

--- sparselearning/core.py
+++ sparselearning/core.py
@@ -180,15 +180,14 @@ def NPB_stable_forward(self, x):
 
 def NPB_residual_forward(self, x, y):
     if self.measure:
+        print(x[1].sum().log(), y[1].sum().log())
         return x[0], x[1] + y[1],  x[2] + y[2]
     if self.training:
-        return x[0], x[1] + y[1], x[2] + y[2]
+        # return x[0], x[1] + y[1], x[2] + y[2]
         if x[0] > y[0]:
             return x[0], x[1] + (y[1] / (x[0]-y[0]).exp()), x[2] + y[2]
-            # return x[0], x[1], x[2] + y[2]
         else:
             return y[0], (x[1] / (y[0]-x[0]).exp()) + y[1], x[2] + y[2]
-            # return y[0], y[1], x[2] + y[2]
     else:
         return self.original_forward(x, y)
 
