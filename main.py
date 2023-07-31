021671e05fd7f98332fea4b30ac193a59769252d
Nguyễn Trí Quân <nguyentriquan99@gmail.com>
2023-07-31 22:30:37 +0700

    dst

--- main.py
+++ main.py
@@ -428,8 +428,18 @@ def main():
                 raise Exception('Unknown optimizer.')
             lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[int(args.epochs / 2) * args.multiplier, int(args.epochs * 3 / 4) * args.multiplier], last_epoch=-1)
             mask.optimizer = optimizer
+        elif args.method == 'score_npb':
+            params = list(model.parameters()) + [m.g for m in model.NPB_modules]
+            if args.optimizer == 'sgd':
+                optimizer = optim.SGD(params,lr=args.lr,momentum=args.momentum,weight_decay=args.l2, nesterov=True)
+            elif args.optimizer == 'adam':
+                optimizer = optim.Adam(params,lr=args.lr,weight_decay=args.l2)
+            else:
+                print('Unknown optimizer: {0}'.format(args.optimizer))
+                raise Exception('Unknown optimizer.')
+            lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[int(args.epochs / 2) * args.multiplier, int(args.epochs * 3 / 4) * args.multiplier], last_epoch=-1)
+            mask.optimizer = optimizer
 
-        print(dict(model.named_parameters()).keys())
         for epoch in range(1, args.epochs*args.multiplier + 1):
             t0 = time.time()
             train(args, model, device, train_loader, optimizer, epoch, mask)
