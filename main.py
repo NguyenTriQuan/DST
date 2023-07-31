6bb6436d1b57c485c89380a9cf985317d0233dcd
Nguyễn Trí Quân <nguyentriquan99@gmail.com>
2023-07-31 22:16:26 +0700

    dst

--- main.py
+++ main.py
@@ -429,7 +429,7 @@ def main():
             lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[int(args.epochs / 2) * args.multiplier, int(args.epochs * 3 / 4) * args.multiplier], last_epoch=-1)
             mask.optimizer = optimizer
 
-        print(model.named_parameters().keys())
+        print(model.named_parameters())
         for epoch in range(1, args.epochs*args.multiplier + 1):
             t0 = time.time()
             train(args, model, device, train_loader, optimizer, epoch, mask)
