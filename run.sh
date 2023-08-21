6b4c142e669adcdcccc6180714d542728fd4628c
Nguyễn Trí Quân <nguyentriquan99@gmail.com>
2023-08-21 14:57:49 +0700

    dst

---
+++ run.sh
@@ -0,0 +1,1 @@
+python main.py --sparse --amp --seed 18 --sparse_init ERK --method npb --alpha 1e-3 --beta 0.05 --l2 0 --multiplier 1 --lr 0.1 --lr_score 1 --density 0.01 --update_frequency 1500 --epochs 250 --model ResNet18 --data cifar10 --decay_frequency 30000 --batch-size 128 --growth random --death magnitude --redistribution none
