222abb7ca21517bc9ffe5f184c5bf6b0e63be007
Nguyễn Trí Quân <nguyentriquan99@gmail.com>
2023-09-10 03:25:15 +0700

    dst

---
+++ tune_node_path.sh
@@ -0,0 +1,5 @@
+for alpha in 10 5 0.5
+    for beta in 10 5 0.5
+do
+    python main.py --sparse --amp --wandb --seed 18 --sparse_init ERK --method npb --alpha $alpha --beta $beta --l2 5.0e-4 --multiplier 1 --lr 0.1 --lr_score 1 --density 0.01 --update_frequency 1500 --epochs 250 --model ResNet18 --data cifar10 --decay_frequency 30000 --batch-size 128 --growth random --death NPB --redistribution none
+done
