4c1df6a9dd3bec212c73c911897cf09ffd5013f9
Nguyễn Trí Quân <nguyentriquan99@gmail.com>
2023-09-10 09:44:46 +0700

    dst

--- tune_path.sh
+++ tune_path.sh
@@ -1,4 +1,4 @@
-for beta in 10 1 0.1 0.01
+for beta in 50 100 150 200
 do
     python main.py --sparse --amp --wandb --seed 18 --sparse_init ERK --method npb --alpha 0 --beta $beta --l2 5.0e-4 --multiplier 1 --lr 0.1 --lr_score 1 --density 0.01 --update_frequency 1500 --epochs 250 --model ResNet18 --data cifar10 --decay_frequency 30000 --batch-size 128 --growth random --death NPB --redistribution none
 done
